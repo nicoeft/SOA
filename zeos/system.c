d6597ac769bb8641e25ee1e780aa4813420330ae
Nico <nfrancisquelo2@gmail.com>
2018-04-07 13:47:46 +0200

    "Testeando TaskSwitch 1"

--- zeos/system.c
+++ zeos/system.c
@@ -109,11 +109,6 @@ int __attribute__((__section__(".text.main")))
    * We return from a 'theorical' call to a 'call gate' to reduce our privileges
    * and going to execute 'magically' at 'usr_main'...
    */
-  
-  if(zeos_ticks>=200){
-    //printk("Zeos task switch");
-    task_switch((union task_union *)idle_task);
-  }
   return_gate(__USER_DS, __USER_DS, USER_ESP, __USER_CS, L_USER_START);
 
   /* The execution never arrives to this point */
