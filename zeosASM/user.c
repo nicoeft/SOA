f9f9b3a7828b358cb13fd23901c5885038c7d507
nico <nfrancisquelo2@gmail.com>
2018-05-09 15:49:17 +0200

    wrokloads y schedxls

--- zeosASM/user.c
+++ zeosASM/user.c
@@ -2,7 +2,6 @@
 
 char buff[256];
 
-int pid;
 
  int fibonacci(int n)
 {
@@ -16,32 +15,28 @@ int pid;
     return 0;
 }
 
-void printStats(void){
+void printStats(int pid){
     struct stats st;
     char buffPid[10];
-    get_stats(getpid(), &st);
-		write(1, "\nPID: ", strlen("\nPID: "));
-		itoa(getpid(), buffPid);
-		write(1,buffPid,strlen(buffPid));
+    get_stats(pid, &st);
+	write(1, "\nPID: ", strlen("\nPID: "));
+	itoa(pid, buffPid);
+	write(1,buffPid,strlen(buffPid));
 		
-		write(1," User: ", strlen(" User: "));
-		itoa(st.user_ticks,buff);
-		write(1,buff,strlen(buff));
+	write(1," User: ", strlen(" User: "));
+	itoa(st.user_ticks,buff);
+	write(1,buff,strlen(buff));
 
-		write(1," System: ", strlen(" System: "));
-		itoa(st.system_ticks,buff);
-		write(1,buff,strlen(buff));
+	write(1," System: ", strlen(" System: "));
+	itoa(st.system_ticks,buff);
+	write(1,buff,strlen(buff));
 
-		write(1," Blocked: ", strlen(" Blocked: "));
-		itoa(st.blocked_ticks,buff);
+	write(1," Blocked: ", strlen(" Blocked: "));
+	itoa(st.blocked_ticks,buff);
     write(1,buff,strlen(buff));
 
     write(1," Ready: ", strlen(" Ready: "));
-		itoa(st.ready_ticks,buff);
-    write(1,buff,strlen(buff));
-
-    write(1," Total: ", strlen(" Total: "));
-		itoa(st.elapsed_total_ticks,buff);
+	itoa(st.ready_ticks,buff);
     write(1,buff,strlen(buff));
 }
 
@@ -50,28 +45,25 @@ void printStats(void){
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
-  set_sched_policy(1); // 0-RR 1-FCFS
+  set_sched_policy(0); // 0-RR 1-FCFS
   int retPid = fork();
   if(retPid !=0) fork();
  /*  CPU 
   for (int num=30; num<35; num++){
 	  fibonacci(num);
-    printStats(); 
   }
-  exit();
-  */
+	*/
   /* I/O 
   for(int i=0;i<5;i++){
     read(0,&buff, 500);
-    printStats();
-  }
-  exit();*/
-  /*  Both*/
+  }*/
+  /*  Both */
     for (int num=30; num<35; num++){
     if(retPid == 0) fibonacci(num);
     else read(0,&buff, 500);
-    printStats(); 
   }
+  printStats(getpid());
+  printStats(0);
   exit();
   
     
